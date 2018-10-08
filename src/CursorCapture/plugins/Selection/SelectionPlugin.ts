515673080e82b89d7bb737beed499d8a5bc767af
Sam Denty <samddenty+gh@gmail.com>
2018-10-08 22:19:57 +0100

    3 hours

--- src/CursorCapture/plugins/Selection/SelectionPlugin.ts
+++ src/CursorCapture/plugins/Selection/SelectionPlugin.ts
@@ -1,36 +1,55 @@
 import { Plugin, Cursor } from '../../..'
+import { isNodeFollowing } from './nodePosition';
 
 export class SelectionPlugin implements Plugin {
   constructor(private cursor: Cursor) {}
 
-  private startRange: Range
+  private range: Range
+
+  private prevLeftToRight: boolean
 
   public mouseMove() {
-    if (this.startRange) {
-      const endRange = document.caretRangeFromPoint(
+    if (this.range) {
+      const {startContainer: currentContainer, startOffset: currentOffset} = document.caretRangeFromPoint(
         this.cursor.x,
         this.cursor.y
       )
 
+      // This logic is bugged, failed to debug
+      // Resets the start offset when selection nodes with different children
+
+      const startSameNode = this.range.startContainer === currentContainer
+      const endSameNode = this.range.endContainer === currentContainer
+
+      const leftToRight = (startSameNode && endSameNode) ? (this.prevLeftToRight ? this.range.startOffset : this.range.endOffset) < currentOffset : isNodeFollowing(currentContainer, this.range.startContainer)
+
+      if (leftToRight) {
+        if (leftToRight !== this.prevLeftToRight) {
+          this.range.setStart(this.range.endContainer, this.range.endOffset)
+        }
+
+        this.range.setEnd(currentContainer, currentOffset)
+      } else {
+        if (leftToRight !== this.prevLeftToRight) {
+          this.range.setEnd(this.range.startContainer, this.range.startOffset)
+        }
+
+        this.range.setStart(currentContainer, currentOffset)
+      }
+
+      this.prevLeftToRight = leftToRight
+
       const selection = window.getSelection()
-      const range = document.createRange()
-
-      range.setStart(
-        this.startRange.startContainer,
-        this.startRange.startOffset
-      )
-      range.setEnd(endRange.endContainer, endRange.endOffset)
-
       selection.removeAllRanges()
-      selection.addRange(range)
+      selection.addRange(this.range)
     }
   }
 
   public mouseDown() {
-    this.startRange = document.caretRangeFromPoint(this.cursor.x, this.cursor.y)
+    this.range = document.caretRangeFromPoint(this.cursor.x, this.cursor.y)
   }
 
   public mouseUp() {
-    this.startRange = null
+    this.range = null
   }
 }
---
+++ src/CursorCapture/plugins/Selection/nodePosition.ts
@@ -0,0 +1,2 @@
+export const isNodeFollowing = (followingNode: Node, target: Node) =>  !!(target.compareDocumentPosition(followingNode) & Node.DOCUMENT_POSITION_FOLLOWING)
+
